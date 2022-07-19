0556950d1eec32c93389e877ef33955865eda5c8
VadSmith <oldfatham@gmail.com>
2022-07-19 15:35:55 +0700

    add real links

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,9 +5,9 @@ function Portfolio() {
     <section className="main__section portfolio">
       <h2 className="main__section-name portfolio__name">Портфолио</h2>
       <ul className="portfolio__list">
-        <li className="portfolio__list-item"><a className="portfolio__link" href="https://github.com/">Статичный сайт</a><img className="portfolio__arrow" src={Arrow} /></li>
-        <li className="portfolio__list-item"><a className="portfolio__link" href="https://github.com/">Адаптивный сайт</a><img className="portfolio__arrow" src={Arrow} /></li>
-        <li className="portfolio__list-item"><a className="portfolio__link" href="https://github.com/">Одностраничное приложение</a><img className="portfolio__arrow" src={Arrow} /></li>
+        <li className="portfolio__list-item"><a className="portfolio__link" href="https://github.com/VadSmith/how-to-learn">Статичный сайт</a><img className="portfolio__arrow" src={Arrow} /></li>
+        <li className="portfolio__list-item"><a className="portfolio__link" href="https://vadsmith.github.io/russian-travel/">Адаптивный сайт</a><img className="portfolio__arrow" src={Arrow} /></li>
+        <li className="portfolio__list-item"><a className="portfolio__link" href="https://github.com/VadSmith/react-mesto-api-full">Одностраничное приложение</a><img className="portfolio__arrow" src={Arrow} /></li>
       </ul>
     </section>
   )
